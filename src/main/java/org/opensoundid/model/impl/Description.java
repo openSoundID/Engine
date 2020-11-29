c5aa4d82d152befaa6e5d099f09e5a41a51f2e0f
jerome <lamourj@free.Fr>
2020-11-29 07:54:03 +0100

    Add flatness descriptor

--- src/main/java/org/opensoundid/model/impl/Description.java
+++ src/main/java/org/opensoundid/model/impl/Description.java
@@ -2,9 +2,19 @@ package org.opensoundid.model.impl;
 
 public class Description {
 	  double[] zero_crossing_rate;
+	  double[] flatness;
 	  double[][] envelope;
 
-	public double[] getZero_crossing_rate() {
+		public double[] getFlatness() {
+			return flatness;
+		}
+
+		public void setFlatness(double[] flatness) {
+			this.flatness = flatness;
+		}
+	  
+	  
+	  public double[] getZero_crossing_rate() {
 		return zero_crossing_rate;
 	}
 
